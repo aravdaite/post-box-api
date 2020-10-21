b0b5d678a25c5ff1c472b880f72cd9159c69958d
aravdaite <aravdaite@gmail.com>
2020-10-21 19:47:21 +0200

    m

--- server.js
+++ server.js
@@ -4,19 +4,6 @@ const errorHandler = require('./middleware/error');
 const cors = require('cors');
 const routes = require('./routes/routes');
 const connectDB = require('./config/db');
-const webpush = require('web-push');
-// const vapidKeys = webpush.generateVAPIDKeys();
-
-webpush.setGCMAPIKey(process.env.GCMAPI_KEY);
-const vapidKeys = {
-	publicKey: process.env.PUBLIC_KEY,
-	privateKey: process.env.PRIVATE_KEY,
-};
-webpush.setVapidDetails(
-	'mailto:myego0@gmail.com',
-	vapidKeys.publicKey,
-	vapidKeys.privateKey
-);
 
 const app = express();
 app.use(express.json());
@@ -26,7 +13,7 @@ connectDB();
 
 app.use((req, res, next) => {
 	res.append('Access-Control-Allow-Origin', process.env.DOMAIN);
-	res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.append('Access-Control-Allow-Headers', 'Content-Type');
 	res.header('Access-Control-Allow-Credentials', 'true');
 	next();
@@ -34,56 +21,7 @@ app.use((req, res, next) => {
 
 //mount routes
 app.use('/api', routes);
-const subLocal = {
-	endpoint:
-		'https://fcm.googleapis.com/fcm/send/egnY8zm3_sw:APA91bF2FTnz-Mq8Rd_2KizYjqWaWgKUGby3llcbFMtYfb1HFA2FkLgfV4K4wccNxdjITDyYCRswqSKiUwMhUN38xPCn2DaR_1tilTr622BLIfmvYSDPOFK9QZ8dVQ03znPBXJwVsslz',
-	expirationTime: null,
-	keys: {
-		p256dh:
-			'BBIZY-MU_eIdgCiaRTYHjQrGfyBDrZ0QU-xeozxofcwbC3QAsRRmlhP2765V1A-zWMFJEVcniD-ZLreDr73AIrw',
-		auth: 'JV_7LUF9Saixt-az9Zk1LQ',
-	},
-};
-app.post('/api/push', (req, res) => {
-	//	return getSubscriptionsFromDatabase()
-	//		.then((subscriptions) => {
-	let promiseChain = Promise.resolve();
-	//for (let i = 0; i < subscriptions.length; i++) {
-	//		const subscription = subscriptions[i];
-	promiseChain = promiseChain.then(() => {
-		triggerPush(subLocal, 'dataToSend');
-	});
-	//}
 
-	//	})
-
-	//.then(() => {
-	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify({ data: { success: true } }));
-	//})
-	//.catch((err) => {
-	//	res.status(500);
-	//	res.setHeader('Content-Type', 'application/json');
-	//	res.send(
-	//		JSON.stringify({
-	//			error: {
-	//				id: 'unable-to-send-messages',
-	//				message: `Failed to send the push ${err.message}`,
-	//			},
-	//		})
-	//	);
-	//	});
-});
-const triggerPush = (subscription, dataToSend) => {
-	return webpush.sendNotification(subscription, dataToSend).catch((err) => {
-		if (err.statusCode === 410) {
-			console.log(err);
-			// return deleteSubscriptionFromDatabase(subscription._id);
-		} else {
-			console.log('Subscription is no longer valid: ', err);
-		}
-	});
-};
 const PORT = process.env.PORT || 5000;
 const server = http.Server(app);
 
