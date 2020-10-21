ae8912a161da0abcc3079338f8cbdaa7c0e9d8b0
aravdaite <aravdaite@gmail.com>
2020-10-21 20:03:00 +0200

    mä

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 
 app.use((req, res, next) => {
 	res.append('Access-Control-Allow-Origin', process.env.DOMAIN);
-	res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 	res.append('Access-Control-Allow-Headers', 'Content-Type');
 	res.header('Access-Control-Allow-Credentials', 'true');
 	next();
